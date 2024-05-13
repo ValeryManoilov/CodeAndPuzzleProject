50da126209ec76fd332c67f7854927bdd1ab06ba
Satlykovs <satlykovs@gmail.com>
2024-05-13 13:33:02 +0300

    feat : lesson models added

---
+++ Model/Data/LessonRating.cs
@@ -0,0 +1,7 @@
+public class LessonRating
+{
+    public int Id { get; set; }
+    public int LessonId { get; set; }
+    public int MarkCount { get; set; }
+    public int Rating { get; set; }
+}
---
+++ Model/Data/UserFavouriteLesson.cs
@@ -0,0 +1,7 @@
+public class UserFavouriteLesson
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int LessonId { get; set;}
+
+}
