58225a5f8bee870ebcbd9657b25f18f6d1e82713
Shashlik <dimova.a.v@mail.ru>
2024-05-15 15:10:46 +0300

    запросы

--- obj/Debug/net8.0/CodeAndPuzzleProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/CodeAndPuzzleProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeAndPuzzleProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1e96c2c215e8b1ba2ff6ed57237137834c6fdee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+261d43175465333781c0d7d2eea5e8c13f9eb46d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeAndPuzzleProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeAndPuzzleProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- reg.js
+++ reg.js
@@ -1,7 +1,7 @@
 const password = document.getElementById("password");
 const confirmPassword = document.getElementById("passwordpovtor");
 const username = document.getElementById("email");
-const button = document.getElementById("vxod");
+const button = document.getElementById("zaregistr");
 
 button.addEventListener('click', () => {
     if (password.value !== confirmPassword.value) {
@@ -12,15 +12,13 @@ button.addEventListener('click', () => {
         return;
     }
 
-    fetch('http://localhost:3000/register', {
+    const formData = new FormData();
+    formData.append('username', username.value);
+    formData.append('password', password.value);
+
+    fetch('http://localhost:5083/api/user/adduser', {
         method: 'POST',
-        body: JSON.stringify({
-            username: username.value,
-            password: password.value
-        }),
-        headers: {
-            'Content-type': 'application/json',
-        }
+        body: formData
     }).then(response => {
         if (response.status === 200) {
             localStorage.setItem('isLoggedIn', 'true');
@@ -34,3 +32,40 @@ button.addEventListener('click', () => {
         }
     });
 });
+
+// const password = document.getElementById("password");
+// const confirmPassword = document.getElementById("passwordpovtor");
+// const username = document.getElementById("email");
+// const button = document.getElementById("vxod");
+
+// button.addEventListener('click', () => {
+//     if (password.value !== confirmPassword.value) {
+//         const errorMessage = document.createElement('p');
+//         errorMessage.textContent = 'Пароли не совпадают';
+//         errorMessage.style.color = 'red';
+//         button.insertAdjacentElement('beforebegin', errorMessage);
+//         return;
+//     }
+
+//     fetch('http://localhost:5083//swagger/api/user/adduser', {
+//         method: 'POST',
+//         body: JSON.stringify({
+//             username: username.value,
+//             password: password.value
+//         }),
+//         headers: {
+//             'Content-type': 'application/json',
+//         }
+//     }).then(response => {
+//         if (response.status === 200) {
+//             localStorage.setItem('isLoggedIn', 'true');
+//             window.location.href = '../html/cabinet.html';
+//             window.localStorage.setItem('username', document.getElementById("email").value);
+//         } else {
+//             const errorMessage = document.createElement('p');
+//             errorMessage.textContent = 'Не удалось зарегистрироваться. Попробуйте еще раз.';
+//             errorMessage.style.color = 'red';
+//             button.insertAdjacentElement('beforebegin', errorMessage);
+//         }
+//     });
+// });
