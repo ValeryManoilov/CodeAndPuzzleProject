e4fcae0a6527922a99294f4560dc73771b452c0a
Shashlik <dimova.a.v@mail.ru>
2024-05-15 23:24:54 +0300

    спасите

--- obj/Debug/net8.0/CodeAndPuzzleProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/CodeAndPuzzleProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeAndPuzzleProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79f992c4ff51dcea921fe82545377064863ae68c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63b83fe878da228b435a155b2282b5a9f5597f9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeAndPuzzleProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeAndPuzzleProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
