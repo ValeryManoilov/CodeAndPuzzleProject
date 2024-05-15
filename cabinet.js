79f992c4ff51dcea921fe82545377064863ae68c
Shashlik <dimova.a.v@mail.ru>
2024-05-15 17:38:02 +0300

    .

--- cabinet.js
+++ cabinet.js
@@ -1,15 +1,15 @@
 document.getElementById('save').addEventListener('click', function() {
-    // Собираем данные из формы
+
     const formData = new FormData();
     formData.append('FirstName', document.getElementById('name').value);
     formData.append('LastName', document.getElementById('sname').value);
     formData.append('Email', document.getElementById('email').value);
     formData.append('UserName', document.getElementById('username').value);
     
-    // const avatarFile = document.getElementById('avatarInput').files[1];
+    // const avatarFile = document.getElementById('avatarInput').files[0];
     // formData.append('avatar', avatarFile);
     
-    // Отправляем запрос на сервер
+
     fetch('http://localhost:5083/api/user/adduser', {
         method: 'POST',
 
